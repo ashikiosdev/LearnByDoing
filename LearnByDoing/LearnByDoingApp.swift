7b96b70e65f982c1d4a4022bdefb4e80002a9323
ashikiosdev <ashik.iosdev@gmail.com>
2022-01-24 11:35:00 +0600

    first commit

---
+++ LearnByDoing/LearnByDoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnByDoingApp.swift
+//  LearnByDoing
+//
+//  Created by os on 24/1/22.
+//
+
+import SwiftUI
+
+@main
+struct LearnByDoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
